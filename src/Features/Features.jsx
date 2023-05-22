f5894b277ba98b88b81e6ae5fe33d586cafe6eee
shanto096 <shanto096@gmail.com>
2023-05-22 22:15:21 +0600

    r

--- src/Features/Features.jsx
+++ src/Features/Features.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Feature from '../Feature/Feature';
 import { Link, useLoaderData } from 'react-router-dom';
 import Data from '../Data';
 
 const Features = () => {
+    const [showMore, setShowMore] = useState(true)
+
+    const handleShowMore=()=>{
+
+        setShowMore(!showMore)
+    }
+
     
     return (
         <div>
@@ -12,12 +19,13 @@ const Features = () => {
            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
            </div>
            <div className='grid md:grid-cols-2 gap-x-20 gap-y-7 md:mx-32 mx-5 mt-10'>
+
                 {
                 Data.map(feature => <Feature key={feature.id} feature = {feature}></Feature>)
                 }
            </div>
            <div className='text-center my-4'>
-           <Link><button className='bg-yellow-200 border-2 border-slate-600 p-2 rounded-md font-bold text-slate-600'>See All Jobs </button></Link>
+           <Link><button onClick={handleShowMore} className='bg-yellow-200 border-2 border-slate-600 p-2 rounded-md font-bold text-slate-600'>See All Jobs </button></Link>
            </div>
         </div>
     );
